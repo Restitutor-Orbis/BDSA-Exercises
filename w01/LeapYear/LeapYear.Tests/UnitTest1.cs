4484aa71b8126c698b37fe34f8f2745df961561b
vson <vson@itu.dk>
2021-09-03 16:44:22 +0200

    Implement initial leap year function

--- w01/LeapYear/LeapYear.Tests/UnitTest1.cs
+++ w01/LeapYear/LeapYear.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Xunit;
 
 namespace LeapYear.Tests
@@ -6,9 +7,27 @@ namespace LeapYear.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void LeapYear_divisibleBy4_true()
         {
+            //Arrange
+            bool expected = true;
 
+            int first = 0;
+            int second = 400;
+            int third  = 1604;
+
+            //Act
+            ArrayList yearList = new ArrayList();
+            yearList.Add(first);
+            yearList.Add(second);
+            yearList.Add(third);
+
+            //Assert
+            foreach(int year in yearList) 
+            {
+                bool actual = Program.isLeapYear(year);
+                Assert.Equal(expected, actual);
+            }
         }
     }
 }
--- w01/LeapYear/LeapYear/Program.cs
+++ w01/LeapYear/LeapYear/Program.cs
@@ -2,11 +2,16 @@
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(":D");
         }
+
+        public static bool isLeapYear(int year) 
+        {
+            return year % 4 == 0;
+        } 
     }
 }
