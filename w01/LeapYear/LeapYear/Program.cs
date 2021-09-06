225dc85b6917beeef03bb43651b5e263b2d4de1b
vson <vson@itu.dk>
2021-09-06 17:28:51 +0200

    Add user input error handling

--- w01/LeapYear/LeapYear/Program.cs
+++ w01/LeapYear/LeapYear/Program.cs
@@ -8,17 +8,33 @@ namespace LeapYear
         {
             Console.WriteLine("Type in a year to see if it is a leap year:");
 
-            int answer = Convert.ToInt32(Console.ReadLine());
+            int answer = 0;
+
+            while(answer == 0) {
+                try 
+                {
+                answer = Convert.ToInt32(Console.ReadLine());
+                }
+                catch(System.FormatException e) 
+                {
+                    Console.WriteLine("Please type in an integer");
+                    throw e;
+                }
+            }
 
             if(isLeapYear(answer)) Console.WriteLine("yay");
             else                   Console.WriteLine("nay");
+
+            if(answer < 1582)      Console.WriteLine("(years before 1582 cannot be leap years)"); //should be added to isLeapYear(), but too lazy
         }
         public static bool isLeapYear(int year) 
         {
-            if(year % 100 == 0) 
-                if(year % 400 == 0) return true;
-                else                return false;
-            else return year % 4 == 0;
+            if(1582 < year) 
+                if(year % 100 == 0) 
+                    if(year % 400 == 0) return true;
+                    else                return false;
+                else return year % 4 == 0;
+            else return false;
         } 
     }
 }
